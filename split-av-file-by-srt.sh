33fdd32ea29e758eff811d3868ac7c1946b182b2
kjoetom <77550697+kjoetom@users.noreply.github.com>
2023-06-01 00:21:29 +0200

    Update split-av-file-by-srt.sh

--- split-av-file-by-srt.sh
+++ split-av-file-by-srt.sh
@@ -2,9 +2,9 @@
 
 if [ $# -eq 0 ]
   then
-    echo "Script to split video files into chunks based on .srt timecodes"
+    echo "Script to split audio or video files into chunks based on .srt timecodes"
     echo ""
-    echo "usage: srt-split.sh [video file] [subtitle file] (optional)[output format]"
+    echo "usage: split-av-file-by-srt [audio-video file] [subtitle file] (optional)[output format]"
     echo "If no output format is supplied, cut files will be saved in the same format as the original file."
     exit 0
 fi
@@ -20,7 +20,7 @@ fileName="${fileName%.*}"
 if [ ! -f "$fileToCut" ]
 then
   echo "ERR: no file found at $fileToCut"
-  echo "usage: srt-split.sh [video file] [subtitle file] (optional)[output format]"
+  echo "usage: split-av-file-by-srt [audio-video file] [subtitle file] (optional)[output format]"
   exit 1
 fi
 
@@ -31,8 +31,8 @@ then
   # create two arrays for the start times and durations of all the clips
   while read -r line ; do
    # extract start and end timecodes from each line of srt file
-   startTime=`echo $line | egrep -o "^[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}"`
-   endTime=`echo $line | egrep -o " [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}"`
+   startTime=`echo $line | egrep -o "^[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}"`
+   endTime=`echo $line | egrep -o " [0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}"`
 
    # format start time for Ffmpeg
    startTimeForFfmpeg[i]=`echo $startTime | sed 's/,/./'`
@@ -46,7 +46,7 @@ then
   done < <(egrep "[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}" "$subtitleFile")
 else
   echo "ERR: no file found at $subtitleFile"
-  echo "usage: srt-split.sh [video file] [subtitle file] (optional)[output format]"
+  echo "usage: split-av-file-by-srt [audio-video file] [subtitle file] (optional)[output format]"
   exit 1
 fi
 echo "Ready to start cutting."
